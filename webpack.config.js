781625361bed1381d70b2e33605c985844b8dfe7
chenchunyong083 <chenchunyong083@lu.com>
2017-01-10 10:57:19 +0800

    webpack demo

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+var path = require('path');
+module.exports = {
+  entry: {
+    main: './main.js',
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    publicPath: './',
+    filename: '[name].js'
+  },
+  module: {
+    loaders:[
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader?presets[]=es2015&presets[]=react'],
+        include: path.join(__dirname, '.')
+      }
+    ]
+  },
+  plugins: [
+     new webpack.DllReferencePlugin({
+      context: '.',
+      manifest: require("./build/bundle.manifest.json"),
+        }),
+  ]
+};
---
+++ webpack.dll.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        bundle: ['react','react-dom'],
+    },
+    output: {
+    	path: './build',
+        filename: '[name].js',
+        library: '[name]_library'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: './build/bundle.manifest.json',
+            name: '[name]_library',
+        })
+    ]
+};
